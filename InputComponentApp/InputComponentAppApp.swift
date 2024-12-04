9e415ea8769148c86b6ccb9918651c8c99179774
muhmmad-zeeshan <rafaq657@gmail.com>
2024-12-05 00:33:04 +0500

    Initial Commit

---
+++ InputComponentApp/InputComponentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputComponentAppApp.swift
+//  InputComponentApp
+//
+//  Created by Muhammad Zeeshan on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct InputComponentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
